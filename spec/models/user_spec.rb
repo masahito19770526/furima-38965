f20c04070a373a7aeeeded2b17d67b6841332ff3
masahito@19770526 <masahito.shimada@enishi-advisory.com>
2023-06-18 16:02:25 +0900

    レビューコメント対応-2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -87,7 +87,7 @@ RSpec.describe User, type: :model do
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank", "First name is invalid")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it 'first_nameが半角では登録できない' do
         @user.first_name = 'aaaa'
@@ -97,7 +97,7 @@ RSpec.describe User, type: :model do
       it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name can't be blank", "Last name is invalid")
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
       it 'last_nameが半角では登録できない' do
         @user.last_name = 'aaaa'
@@ -107,7 +107,7 @@ RSpec.describe User, type: :model do
       it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana can't be blank", "First name kana is invalid")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
       it 'first_name_kanaが半角では登録できない' do
         @user.first_name_kana = 'aaaa'
@@ -117,7 +117,7 @@ RSpec.describe User, type: :model do
       it 'last_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana can't be blank", "First name kana is invalid")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
       it 'last_name_kanaが半角では登録できない' do
         @user.first_name_kana = 'aaaaa'
