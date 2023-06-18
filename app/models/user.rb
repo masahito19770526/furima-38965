dd195cbb89e0b60b1b8d33a37f28c3c2f57ba73a
masahito@19770526 <masahito.shimada@enishi-advisory.com>
2023-06-18 16:16:48 +0900

    レビューコメント対応-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,10 @@ class User < ApplicationRecord
 # has_many :items
 # has_many :orders 
 
+VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates :password, format: { with: VALID_PASSWORD_REGEX, }
+
 with_options presence: true do 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, }
   validates :nickname, uniqueness: true
   validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
